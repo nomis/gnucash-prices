f2f7a9c885d8396a293d3ffb608f73ceecf6aa08
Simon Arlott <sa.me.uk>
2019-09-13 19:21:39 +0100

    Don't ignore newer prices than the one we have, even if they're out of date

--- gnucash-prices.py
+++ gnucash-prices.py
@@ -147,7 +147,7 @@ def update_prices(session, base_currency, offset, all_commodities, currencies, p
 			else:
 				result["ts"] = tzlocal.get_localzone().localize(result["ts"])
 
-		if result["ts"].date() < now_adjusted:
+		if result["ts"].date() <= prices[key]:
 			logging.warn("Ignoring old data for %s/%s", key[0], key[1])
 			result = None
 
